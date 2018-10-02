43e37069dffd56e94576a82586eceb9c1b62a3b8
Tuna Han Salih MERAL <tunahansalih@Tunas-MacBook-Pro.local>
2018-10-02 13:10:17 +0300

    gotignore

--- Task1.py
+++ Task1.py
@@ -4,6 +4,8 @@ import numpy as np
 
 IMAGE_FILE = '/Users/tunahansalih/School/ComputerVision/SkinColorDetection/Images/Original Images/img_001.jpg'
 
-img = cv2.imread(IMAGE_FILE, 0)
+img = cv2.imread(IMAGE_FILE, 1)
 
 cv2.imshow('Original Image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
