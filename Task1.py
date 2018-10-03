f81a684e2455cddf6bb79d7f6be8d6c4a615120f
tunahansalih <tunahansmeral@gmail.com>
2018-10-03 17:24:27 +0300

    picture output

--- Task1.py
+++ Task1.py
@@ -1,18 +1,20 @@
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
-IMAGE_FILE = '/Users/tunahansalih/School/ComputerVision/SkinColorDetection/Images/Original Images/img_001.jpg'
-
-img = cv2.imread(IMAGE_FILE)
+IMAGE_FILE = os.path.join('Images', 'Original Images', 'img_001.jpg')
 
 # OpenCV uses BGR instead og RGB, that's why channels are different with matplotlib
-def display_image(img):
+def display_image(img, bgr=True):
     plt.figure()
-    plt.imshow(img)
+    if bgr:
+        plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    else:
+        plt.imshow(img)
     plt.show()
+         
     
-
 def get_single_channel(img, channel_index):
     single_channel = np.zeros_like(img)
     single_channel[:,:,channel_index] = img[:,:,channel_index]
@@ -28,22 +30,31 @@ def create_histogram(img, channel):
 def map_hue(img, max_hue_val=179):
      return (img * (max_hue_val / 255)).astype(int)
         
-    
+img = cv2.imread(IMAGE_FILE)
+
 img_r = get_single_channel(img, 2)
 img_g = get_single_channel(img, 1)
 img_b = get_single_channel(img, 0)
 
 cv2.imwrite('img_001_r.jpg', img_r)
+display_image(img_r)
 cv2.imwrite('img_001_g.jpg', img_g)
+display_image(img_g)
 cv2.imwrite('img_001_b.jpg', img_b)
+display_image(img_b)
 
 img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
 img_h = map_hue(get_single_channel(img_hsv, 0))
-cv2.imwrite('img_001_h.jpg', img_h)
 img_s = get_single_channel(img_hsv, 1)
-cv2.imwrite('img_001_s.jpg', img_s)
 img_v = get_single_channel(img_hsv, 2)
+
+cv2.imwrite('img_001_h.jpg', img_h)
+display_image(img_h, bgr=False)
+cv2.imwrite('img_001_s.jpg', img_s)
+display_image(img_s, bgr=False)
 cv2.imwrite('img_001_v.jpg', img_v)
+display_image(img_v, bgr=False)
+
 
 
